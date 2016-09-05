299495e87bf914ba4ed4a5d692bdd7d9e00b1fab
John Freeman <john.freeman.opensource@gmail.com>
2016-09-05 23:19:53 +0100

    Upgraded to Molecule 1.10.3
    Fixed flake8 linter warnings.

--- tests/test_role.py
+++ tests/test_role.py
@@ -5,5 +5,7 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 def test_dockbarx_config(Sudo, Command):
     # Need to -set-home when using sudo for gconftool-2 to work
-    output = Command.check_output("sudo --user=test_usr --set-home gconftool-2 --get /apps/dockbarx/launchers")
+    output = Command.check_output("sudo %s --set-home gconftool-2 --get %s",
+                                  '--user=test_usr',
+                                  '/apps/dockbarx/launchers')
     assert 'test-app;/usr/share/applications/test-app.desktop' in output
